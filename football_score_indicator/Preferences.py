47f1f80e6d5f7a5682bed881442d49787419700c
rubyace71697 <kukreja34@hotmail.com>
2016-08-16 16:45:06 +0530

    changes in preferences window

--- football_score_indicator/Preferences.py
+++ football_score_indicator/Preferences.py
@@ -1,43 +1,70 @@
-from gi.repository import Gtk,Gdk
-from configuration import Configurations
-#import gtk.gdk
-import sys
+#!usr/bin/env python
 
-class PreferencesWindow(Gtk.Window):
+import sys
+import json
+import gi.repository
+
+from gi.repository import Gtk,GObject,Gdk,GtkSource
+from gi.repository import Pango
+from gi.repository import AppIndicator3 as appindicator
+from configuration import Configurations
+
+
+from os.path import expanduser
+import glob
+import os
+
+""" we will remove it later"""
+import ConfigParser
+import re
+
+""" yet learning login """
+import logging
+logging.basicConfig(level = logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+home = expanduser('~')
+
+class PreferencesWindow:
 
     def __init__(self):
+        logger.debug("Initializing PREFERENCES Object")
+        self.builder = Gtk.Builder()
+        d = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+        #print d
+        d = os.path.join(d,"Preferences.glade")
+        #print d
+        self.builder.add_from_file(d)
+        self.preferences_window = self.builder.get_object("preferences_window")
+        self.button1 = self.builder.get_object('radiobutton1')
+        self.button2 = self.builder.get_object('radiobutton2')
+        self.button3 = self.builder.get_object('checkbutton1')
+        self.preferences_window.set_can_focus(True)
+        self.preferences_window.connect('delete_event',self.hide)
+
+        self.button1.connect('toggled',self.on_button_toggled,'1')
+        self.button2.connect('toggled',self.on_button_toggled,'2')
+        self.button3.connect('toggled',self.on_button_toggled,'3')
 
 
+
+    def hide(self,widget,event):
+        GObject.idle_add(self.preferences_window.hide)
+        return True
+
+    def readConfigurations(self):
+    	logger.debug("READING Configurations")
         self.config = Configurations().readConfigurations()
-        Gtk.Window.__init__(self, title="Preferencees")
-        #self.set_icon(gdk.new_from_image(sys.prefix + '/share/icons/hicolor/24x24/apps/football.png'))
-        self.set_icon_from_file(sys.prefix + '/share/icons/hicolor/24x24/apps/football.png')
-        self.set_border_width(10)
-        hbox = Gtk.Box(spacing=6)
-        self.add(hbox)
 
-        self.button1 = Gtk.RadioButton.new_with_label_from_widget(None, "Show Only Live Matches")
+    def setConfigurations(self):
+    	logger.debug("SETTING Configurations")
         self.button1.set_active(self.config['live_matches'])
-        
-        hbox.pack_start(self.button1, False, False, 0)
-
-        self.button2 = Gtk.RadioButton.new_from_widget(self.button1)
-        self.button2.set_label("Show all matches")
         self.button2.set_active(self.config['all_matches'])
-        
-        hbox.pack_start(self.button2, False, False, 0)
-
-        self.button3 = Gtk.CheckButton("Hide Leagues")
         self.button3.set_active(self.config['hide_leauges'])
-        self.button3.connect("toggled", self.on_button_toggled, "3")
-        hbox.pack_start(self.button3, False, False, 0)
-        self.button1.connect("toggled", self.on_button_toggled, "1")
-        self.button2.connect("toggled", self.on_button_toggled, "2")
-        self.connect("destroy",self.exit)
-
-    def exit(self,widget):
-        self.close()
+ 
     def on_button_toggled(self, button, name):
+    	logger.debug("Button toggled")
         if button.get_active():
             state = "on"
         else:
@@ -51,8 +78,17 @@ class PreferencesWindow(Gtk.Window):
         Configurations().writeConfigurations(self.config)
         self.callback()
 
+
+
     def display(self,func):
+    	logger.debug(" In DISPLAY Function")
+        self.readConfigurations()
+        GObject.idle_add(self.setConfigurations)
         self.callback = func
-        self.show_all()
+        GObject.idle_add(self.preferences_window.grab_focus)
+
+
+        GObject.idle_add(self.preferences_window.show_all)
+    
 
 
